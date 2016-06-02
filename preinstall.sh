2bd5ae6c6796564306c52e8c9c2d1c18ca172e09
陈积 <chenji@le.com>
2016-06-02 18:12:37 +0800

    livips
    Signed-off-by: 陈积 <chenji@le.com>

--- preinstall.sh
+++ preinstall.sh
@@ -21,15 +21,17 @@ openslide_version_latest_patch=1
 
 install_libvips_from_source() {
   echo "Compiling libvips $vips_version_latest_major_minor.$vips_version_latest_patch from source"
-  curl -O http://www.vips.ecs.soton.ac.uk/supported/$vips_version_latest_major_minor/vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
-  tar zvxf vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
-  cd vips-$vips_version_latest_major_minor.$vips_version_latest_patch
+#  curl -O http://www.vips.ecs.soton.ac.uk/supported/$vips_version_latest_major_minor/vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
+#  tar zvxf vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
+#  cd vips-$vips_version_latest_major_minor.$vips_version_latest_patch
+  tar zxvf vips-8.3.1.tar.gz
+  cd vips-8.3.1 
   CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" ./configure --disable-debug --disable-docs --disable-static --without-magick --disable-dependency-tracking --enable-cxx=yes --without-python --without-orc --without-fftw $1
   make -j4
   make install
   cd ..
   rm -rf vips-$vips_version_latest_major_minor.$vips_version_latest_patch
-  rm vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
+#  rm vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
   ldconfig
   echo "Installed libvips $(PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --modversion vips)"
 }
